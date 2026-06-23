ded83b27ade921bc87bb98d091e35b2f2b552752
Zakariya <pzakariya@mail.ru>
2026-06-23 17:42:46 +0500

    add dto ta base entity

---
+++ internal/usecase/dto/auth/user.go
@@ -0,0 +1,32 @@
+package auth
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type GetUser struct {
+	ID        uuid.UUID
+	Email     string
+	Password  string
+	Login     string
+	Role      string
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type CreateUser struct {
+	Email    string
+	Password string
+	Login    string
+	Role     string
+}
+
+type UpdateUser struct {
+	ID       uuid.UUID
+	Email    *string
+	Password *string
+	Login    *string
+	Role     *string
+}
