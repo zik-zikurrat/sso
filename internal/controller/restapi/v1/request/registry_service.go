78435207cc849bb3ea9e2b471337c2aeaf99d32d
zakariya <polevchshikovzak@gmail.com>
2026-07-04 23:29:03 +0500

    WIP: little refactor to make code more golang idiomatic

--- internal/controller/restapi/v1/request/registry_service.go
+++ internal/controller/restapi/v1/request/registry_service.go
@@ -3,3 +3,14 @@ package request
 type UpdateService struct {
 	Name *string `json:"name"`
 }
+
+type CreateEndpoint struct {
+	Method string `json:"method" validate:"required"`
+	URL    string `json:"url"    validate:"required"`
+	Secure bool   `json:"secure"`
+}
+
+type CreateService struct {
+	Name      string           `json:"name"      validate:"required"`
+	Endpoints []CreateEndpoint `json:"endpoints" validate:"required,dive"`
+}
