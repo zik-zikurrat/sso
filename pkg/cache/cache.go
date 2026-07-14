e61a689b2dd4b676a0960db5ee7e5de5b39ec225
zakariya <polevchshikovzak@gmail.com>
2026-07-14 14:54:03 +0500

    redis client

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -1,1 +1,49 @@
 package cache
+
+import (
+	"context"
+	"log/slog"
+	"sso/internal/config"
+	"sso/internal/helper"
+	"time"
+
+	goredis "github.com/redis/go-redis/v9"
+)
+
+type Redis struct {
+	log    *slog.Logger
+	Client *goredis.Client
+}
+
+func New(cfg *config.Config, l *slog.Logger) (*Redis, error) {
+	client := goredis.NewClient(&goredis.Options{
+		Addr:         helper.GetRedisAddr(cfg),
+		Password:     cfg.Cache.PASSWORD,
+		DB:           cfg.Cache.DB,
+		DialTimeout:  5 * time.Second, // ожидание установления tcp соединения
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 3 * time.Second,
+	})
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := client.Ping(ctx).Err(); err != nil {
+		l.Error("failed to ping redis", slog.String("error", err.Error()))
+		return nil, err
+	}
+	redis := &Redis{
+		log:    l,
+		Client: client,
+	}
+	return redis, nil
+}
+
+func (r *Redis) Close() error {
+	if r.Client != nil {
+		err := r.Client.Close()
+		if err != nil {
+			r.log.Error("failed to close redis client", slog.String("error", err.Error()))
+			return err
+		}
+	}
+	return nil
+}
